c95177120f65878217e5f837cbee02bc5529d689
Jose Zalacain <Jose Zalacain@10.11.0.25>
2018-12-28 20:21:56 -0400

    Introduce Prolog Builders

---
+++ src/main/java/org/logicware/prolog/AbstractBuilder.java
@@ -0,0 +1,79 @@
+/*
+ * #%L
+ * prolobjectlink-jpi
+ * %%
+ * Copyright (C) 2012 - 2018 WorkLogic Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.logicware.prolog;
+
+/**
+ * 
+ * @author Jose Zalacain
+ * @since 1.0
+ */
+public abstract class AbstractBuilder implements PrologBuilder {
+
+	protected final PrologEngine engine;
+	protected final StringBuilder builder;
+
+	public AbstractBuilder(PrologEngine engine) {
+		this.builder = new StringBuilder();
+		this.engine = engine;
+	}
+
+	public PrologEngine getEngine() {
+		return engine;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PrologQueryBuilder other = (PrologQueryBuilder) obj;
+		if (engine == null) {
+			if (other.engine != null)
+				return false;
+		} else if (!engine.equals(other.engine)) {
+			return false;
+		}
+		if (builder == null) {
+			if (other.builder != null)
+				return false;
+		} else if (!builder.toString().equals(other.builder.toString())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((engine == null) ? 0 : engine.hashCode());
+		result = prime * result + ((builder == null) ? 0 : builder.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "" + builder + "";
+	}
+
+}
---
+++ src/main/java/org/logicware/prolog/PrologQueryBuilder.java
@@ -0,0 +1,206 @@
+/*
+ * #%L
+ * prolobjectlink-jpi
+ * %%
+ * Copyright (C) 2012 - 2018 WorkLogic Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.logicware.prolog;
+
+/**
+ * 
+ * @author Jose Zalacain
+ * @since 1.0
+ */
+public final class PrologQueryBuilder extends AbstractBuilder implements PrologBuilder {
+
+	public PrologQueryBuilder(PrologEngine engine) {
+		super(engine);
+	}
+
+	public PrologQueryBuilder begin(String functor, int... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder begin(String functor, long... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder begin(String functor, double... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder begin(String functor, PrologTerm... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(PrologTerm body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(String functor, int... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(String functor, long... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(String functor, double... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(String functor, PrologTerm... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(PrologTerm left, String operator, int right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(int left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(PrologTerm left, String operator, long right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(long left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(PrologTerm left, String operator, double right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(double left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder comma(PrologTerm left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(PrologTerm term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(String functor, int... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(String functor, long... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(String functor, double... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(String functor, PrologTerm... arguments) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(PrologTerm left, String operator, int right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(int left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(PrologTerm left, String operator, long right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(long left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(PrologTerm left, String operator, double right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(double left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQueryBuilder semicolon(PrologTerm left, String operator, PrologTerm right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrologQuery dot() {
+		String q = "" + builder + "";
+		return engine.query(q);
+	}
+
+	public String getQueryString() {
+		return "" + builder + "";
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PrologEngine getEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
