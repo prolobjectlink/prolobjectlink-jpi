10e2f77ca3e53cea8d6a51ecea77e635de38c7ef
jzalacainllanes <jzalacainllanes@gmail.com>
2021-06-25 01:23:52 -0400

    Change license from MIT to BSD

--- src/main/java/io/github/prolobjectlink/prolog/PrologMethod.java
+++ src/main/java/io/github/prolobjectlink/prolog/PrologMethod.java
@@ -4,28 +4,40 @@
  * %%
  * Copyright (C) 2020 - 2021 Prolobjectlink Project
  * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
  * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Prolobjectlink Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
 package io.github.prolobjectlink.prolog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class PrologMethod extends AbstractClause implements PrologClause {
+
+	private final List<PrologClause> subrutines = new ArrayList<PrologClause>();
 
 	/**
 	 * Create a new fact clause. A fatc clause is only represented by clause head
@@ -65,6 +77,107 @@ class PrologMethod extends AbstractClause implements PrologClause {
 
 	public final PrologIndicator getPrologIndicator() {
 		return new DefaultPrologIndicator(getFunctor(), getArity());
+	}
+
+	public List<PrologClause> getSubrutines() {
+		return subrutines;
+	}
+
+	public final void addMethod(PrologClause method) {
+		subrutines.add(method);
+	}
+
+	/**
+	 * Create a new fact clause. A fatc clause is only represented by clause head
+	 * and no have clause body. The body for this clause type is null. The other
+	 * parameters are boolean clause properties. If a clause have any of this
+	 * properties specify with true value.
+	 * 
+	 * 
+	 * @param head          clause head
+	 * @param dynamic       true if clause is dynamic, false otherwise
+	 * @param multifile     true if clause is multifile, false otherwise
+	 * @param discontiguous true if clause is discontiguous, false otherwise
+	 * @since 1.1
+	 */
+	public final PrologClause addMethod(PrologTerm head, boolean dynamic, boolean multifile, boolean discontiguous) {
+		PrologMethod method = new PrologMethod(provider, head, dynamic, multifile, discontiguous);
+		subrutines.add(method);
+		return method;
+	}
+
+	/**
+	 * Create a new rule clause. A rule clause is represented by clause head and
+	 * body. The other parameters are boolean clause properties. If a clause have
+	 * any of this properties specify with true value.
+	 * 
+	 * 
+	 * @param head          clause head
+	 * @param body          clause body
+	 * @param dynamic       true if clause is dynamic, false otherwise
+	 * @param multifile     true if clause is multifile, false otherwise
+	 * @param discontiguous true if clause is discontiguous, false otherwise
+	 * @since 1.1
+	 */
+	public final PrologClause addMethod(PrologTerm head, PrologTerm body, boolean dynamic, boolean multifile,
+			boolean discontiguous) {
+		PrologMethod method = new PrologMethod(provider, head, body, dynamic, multifile, discontiguous);
+		subrutines.add(method);
+		return method;
+	}
+
+	protected final void removeMethod(PrologClause method) {
+		subrutines.remove(method);
+	}
+
+	public final void addFunction(PrologClause function) {
+		subrutines.add(function);
+	}
+
+	/**
+	 * Create a new fact clause. A fatc clause is only represented by clause head
+	 * and no have clause body. The body for this clause type is null. The other
+	 * parameters are boolean clause properties. If a clause have any of this
+	 * properties specify with true value.
+	 * 
+	 * 
+	 * @param head          clause head
+	 * @param result        function result
+	 * @param dynamic       true if clause is dynamic, false otherwise
+	 * @param multifile     true if clause is multifile, false otherwise
+	 * @param discontiguous true if clause is discontiguous, false otherwise
+	 * @since 1.1
+	 */
+	public final PrologClause addFunction(PrologTerm head, PrologTerm result, boolean dynamic, boolean multifile,
+			boolean discontiguous) {
+		PrologFunction function = new PrologFunction(provider, head, result, dynamic, multifile, discontiguous);
+		subrutines.add(function);
+		return function;
+	}
+
+	/**
+	 * Create a new rule clause. A rule clause is represented by clause head and
+	 * body. The other parameters are boolean clause properties. If a clause have
+	 * any of this properties specify with true value.
+	 * 
+	 * 
+	 * @param head          clause head
+	 * @param body          clause body
+	 * @param result        function result
+	 * @param dynamic       true if clause is dynamic, false otherwise
+	 * @param multifile     true if clause is multifile, false otherwise
+	 * @param discontiguous true if clause is discontiguous, false otherwise
+	 * @since 1.1
+	 */
+	public final PrologClause addFunction(PrologTerm head, PrologTerm body, PrologTerm result, boolean dynamic,
+			boolean multifile, boolean discontiguous) {
+		PrologFunction function = new PrologFunction(provider, head, body, result, dynamic, multifile, discontiguous);
+		subrutines.add(function);
+		return function;
+	}
+
+	protected final void removFunction(PrologClause function) {
+		subrutines.remove(function);
 	}
 
 }
--- src/main/java/io/github/prolobjectlink/prolog/ReflectionUtil.java
+++ src/main/java/io/github/prolobjectlink/prolog/ReflectionUtil.java
@@ -4,23 +4,30 @@
  * %%
  * Copyright (C) 2019 Prolobjectlink Project
  * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
  * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Prolobjectlink Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
 package io.github.prolobjectlink.prolog;
@@ -30,53 +37,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class ReflectionUtil {
-
-	public static boolean isFinal(Field field) {
-		int modifiers = field.getModifiers();
-		return Modifier.isFinal(modifiers);
-	}
-
-	public static boolean isStatic(Field field) {
-		int modifiers = field.getModifiers();
-		return Modifier.isStatic(modifiers);
-	}
-
-	/**
-	 * Check if the given field is {@code this} result of inner classes
-	 * instantiation.
-	 * 
-	 * @param field field to check if is Synthetic
-	 * @return true if the given field is Synthetic false in otherwise
-	 * @since 1.0
-	 */
-	public static boolean isSynthetic(Field field) {
-		return field.isSynthetic();
-	}
-
-	public static boolean isTransient(Field field) {
-		int modifiers = field.getModifiers();
-		return Modifier.isTransient(modifiers);
-	}
-
-	/**
-	 * Check if the given field
-	 * {@code !ObjectReflector#isTransient(Field)&&!ObjectReflector#isSynthetic(Field)}
-	 * 
-	 * @param field field to check if is Persistent
-	 * @return true if the given field is Persistent false in otherwise
-	 * @since 1.0
-	 */
-	public static boolean isPersistent(Field field) {
-		return !isTransient(field) && !isSynthetic(field);
-	}
-
-	public static boolean isStaticAndFinal(Field field) {
-		return isStatic(field) && isFinal(field);
-	}
 
 	/**
 	 * Check if the given java class member (filed, method or constructor) is
@@ -100,10 +63,6 @@ public class ReflectionUtil {
 		if (!isAccessible(member)) {
 			member.setAccessible(false);
 		}
-	}
-
-	public static <O> Class<O> classOf(O o) {
-		return (Class<O>) o.getClass();
 	}
 
 	public static Class<?> classForName(String className) {
